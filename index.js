3c7fb515f3b44f528f60cfa16d24f02521719bef
Lucas Barrozo <barrozo.lucasp@gmail.com>
2024-07-10 18:34:11 +0100

    Lucas js data types lab

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -21,38 +19,35 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let newPart1 =
+  part1.slice(0, part1.length - 1) +
+  part1.slice(part1.length - 1).toUpperCase();
 
+let newPart2 =
+  part2.slice(0, part2.length - 1) +
+  part2.slice(part2.length - 1).toUpperCase();
+
+const result = newPart1 + newPart2;
 
 // Print the cameLtaiL-formatted string
-
-
-
-
+console.log(result);
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
-
+let tipAmount = billTotal * (15 / 100);
 // Print out the tipAmount
-
-
-
-
+console.log(tipAmount);
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+let randomNumber = Math.floor(Math.random() * (11 - 1)) + 1;
 // Print the generated random number
-
-
-
+console.log(Math.round(randomNumber));
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -62,15 +57,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
